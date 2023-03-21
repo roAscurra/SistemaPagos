10a24a4a256f037580e1ae68c2f97e1deecead05
rominaascurra <rominaascurra4887@gmail.com>
2023-03-21 20:13:52 -0300

    Consumiendo precio de conversion de USDT a ARS desde cryptocompare

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ function consultarAPI(){
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=USDT&tsyms=ARS`;
     fetch(url)
         .then(resultado => resultado.json())
-        .then(resultadoJson => {resultadoJson.RAW.USDT.ARS.PRICE;
+        .then(resultadoJson => {precioCompra=resultadoJson.RAW.USDT.ARS.PRICE;
         })
         .catch(error => console.log(error));
 }
