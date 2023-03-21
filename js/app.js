667d51228564327abd21addf4a42739a3d75ab32
rominaascurra <rominaascurra4887@gmail.com>
2023-03-21 20:18:58 -0300

    Restando el - del precio

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ function consultarAPI(){
     const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=USDT&tsyms=ARS`;
     fetch(url)
         .then(resultado => resultado.json())
-        .then(resultadoJson => {precioCompra=resultadoJson.RAW.USDT.ARS.PRICE;
+        .then(resultadoJson => {
+            precioCompra=(resultadoJson.RAW.USDT.ARS.PRICE)-(resultadoJson.RAW.USDT.ARS.PRICE*1/100);
         })
         .catch(error => console.log(error));
 }
