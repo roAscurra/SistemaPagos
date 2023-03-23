5819ee94c97a97ca6c2da53186aaac0d25443cee
rominaascurra <rominaascurra4887@gmail.com>
2023-03-23 15:50:09 -0300

    Conectando a la base de datos de firebase

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
+    //import { collection, getFirestore, addDoc, getDocs, onSnapshot, orderBy } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js"
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyDOlkR26ROtL9isCuvIgqOrCbo4ifhNeJA",
+      authDomain: "crud-543f2.firebaseapp.com",
+      projectId: "crud-543f2",
+      storageBucket: "crud-543f2.appspot.com",
+      messagingSenderId: "31213581685",
+      appId: "1:31213581685:web:6cd86650cb0d90381e0f3c"
+    };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
